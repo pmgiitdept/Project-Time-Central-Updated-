22392ed878afe8a87b31d51f46e0073a96995370
pmgiitdept <pmgi.itdept@gmail.com>
2026-03-16 16:25:27 +0800

    fixed period in backend

--- backend_project/files/services/payroll_comparator.py
+++ backend_project/files/services/payroll_comparator.py
@@ -31,6 +31,7 @@ def compare_dtr_with_payroll_pdf(dtr_file, log_debug=None):
 
         formats = [
             "%Y-%m-%d",
+            "%d/%m/%Y",
             "%m/%d/%Y",
             "%m-%d-%Y",
             "%b %d, %Y",
